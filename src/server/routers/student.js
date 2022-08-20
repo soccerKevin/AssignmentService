455a41b662c8e985c9321d60877430673b798152
soccerKevin <soccerkevint11@gmail.com>
2022-08-20 15:05:15 -0700

    safe query

--- src/server/routers/student.js
+++ src/server/routers/student.js
@@ -5,7 +5,7 @@ const { pool } = connect
 const router = express.Router()
 
 router.get('/:id', async ({ params: { id } }, res) => {
-  const { rows } = await pool.query(`SELECT * FROM student WHERE id=${id};`)
+  const { rows } = await pool.query("SELECT * FROM student WHERE id=$1;", [id])
   res.send(rows[0])
 })
 
