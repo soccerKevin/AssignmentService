9ad0a68ad1e4f8670603233f35bb4b3c23de4cda
soccerKevin <soccerkevint11@gmail.com>
2022-08-21 18:15:11 -0700

    change route to registration

--- src/server/routers/registration.js
+++ src/server/routers/registration.js
@@ -3,7 +3,7 @@ import { connect } from 'sa/db/index.js'
 const { pool } = connect
 const router = express.Router()
 
-export const path = '/register'
+export const path = '/registration'
 export const acceptedParams = ['courseId', 'studentId']
 
 router.post('', async ({ accepted: { keys, vars, values } }, res) => {
